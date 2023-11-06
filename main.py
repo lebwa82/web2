6b8b2dbcac176c19cb0a5d413a449ca5fe14f727
Dmitry Parfenov <lebwa82@gmail.com>
2023-11-06 22:06:22 +0300

    hw8

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tornado import web, ioloop
+from tornado import web, ioloop, websocket
 import tornado
 import asyncio
 
@@ -16,11 +16,24 @@ class GetFile(tornado.web.RequestHandler):
         count += 1
         self.render("hw1.html")
 
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
 
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/hw", GetFile),
-        (r"/count", CountRequests)
+        (r"/count", CountRequests),
+        (r"/websocket", EchoWebSocket),
     ])
     app.listen('8888')
     tornado.ioloop.IOLoop.current().start()
