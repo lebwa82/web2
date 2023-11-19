b2b4df9d0862f3958f94e1e4c9ba48d59832e14f
Dmitry Parfenov <lebwa82@gmail.com>
2023-11-20 01:23:03 +0300

    hw10

---
+++ hw10/producer/producer.py
@@ -0,0 +1,15 @@
+import pika
+from myproto_pb2 import MyRequest, MyResponse, IdMessage
+
+conn_params = pika.ConnectionParameters('localhost', 5672)
+connection = pika.BlockingConnection(conn_params)
+channel = connection.channel()
+
+#channel.queue_declare(queue='first-queue')
+
+message = IdMessage(id = 123, hash = "12321")
+message22 = message.SerializeToString()
+channel.basic_publish(exchange='', routing_key='first-queue', body=message22)
+print("sent ", message)
+    
+#connection.close()
