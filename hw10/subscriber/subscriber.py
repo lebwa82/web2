b18d178265fb1602366d5489557ce9727ce9e588
Dmitry Parfenov <lebwa82@gmail.com>
2023-12-03 21:13:14 +0300

    update hw10

--- hw10/subscriber/subscriber.py
+++ hw10/subscriber/subscriber.py
@@ -1,17 +1,22 @@
 import pika
 from myproto_pb2 import MyRequest, MyResponse, IdMessage
+import time 
 
-conn_params = pika.ConnectionParameters('localhost')
+print("i am sub")
+time.sleep(5)
+conn_params = pika.ConnectionParameters('rabbitmq')
 connection = pika.BlockingConnection(conn_params)
 channel = connection.channel()
 
-channel.queue_declare(queue='first-queue')
+#channel.queue_declare(queue='first-queue')
 
 def callback(ch, method, properties, body):
     message = IdMessage()
     message.ParseFromString(body)
     print("body = ", message.id, message.hash)
 
+print("присоединился к каналу")
 channel.basic_consume(on_message_callback = callback, queue='first-queue', auto_ack=True)
-
+print("начал слушать")
 channel.start_consuming()
+print("слушаю...")
