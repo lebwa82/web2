981fd46250c35d9f4af66f13a994cc3e602f2280
Dmitry Parfenov <lebwa82@gmail.com>
2023-12-04 14:11:02 +0300

    fix5

--- hw11/producer.py
+++ hw11/producer.py
@@ -1,5 +1,5 @@
 import pika
-import myproto_pb2
+from myproto_pb2 import IdMessage # pylint: disable=no-name-in-module
 # time.sleep(5)
 conn_params = pika.ConnectionParameters('rabbitmq', 5672)
 connection = pika.BlockingConnection(conn_params)
@@ -7,7 +7,7 @@ channel = connection.channel()
 
 channel.queue_declare(queue='first-queue')
 
-message = myproto_pb2.IdMessage(id = 123, hash = "12321")
+message = IdMessage(id = 123, hash = "12321")
 message22 = message.SerializeToString()
 channel.basic_publish(exchange='', routing_key='first-queue', body=message22)
 #print("sent ", message)
